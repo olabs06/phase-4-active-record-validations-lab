ad4ae65cd4083ba5690e47b9a91bd15ef37e89fb
Simeon <olabs06@yahoo.com>
2022-10-30 22:09:05 +0100

    modifying authors and post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, exclusion: {in: %w(True Facts)}
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction), message: "is not included in the list"}
+    validate :clickbaity
+
+      
+    def clickbaity
+        if not match_pattern? title
+            errors.add(:title, "is not click baity enough!!!!")
+        end
+    end
+
+    private 
+
+    def match_pattern? title
+        /Won't Believe|Secret|Top \d|Guess/i.match?(title)
+    end
 end
